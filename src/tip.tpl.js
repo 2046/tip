dfee9e4ce215b768be831aec39cdf68ecb082951
hanfeizi <x@xxxx.io>
2014-10-08 19:05:51 +0800

    init

--- src/tip.tpl.js
+++ src/tip.tpl.js
@@ -1,13 +1,9 @@
 module.exports = [
     '<div class="widget-tip">',
-    '   <div class="widget-tip-shadow">',
-    '       <div class="widget-tip-container">',
-    '           <div class="widget-tip-arrow">',
-    '               <em></em>',
-    '               <span></span>',
-    '           </div>',
-    '           <div class="widget-tip-content" data-id="content"></div>',
-    '       </div>',
+    '   <div class="widget-tip-arrow">',
+    '       <em></em>',
+    '       <span></span>',
     '   </div>',
+    '   <div class="widget-tip-content" data-id="content"></div>',
     '</div>'
 ].join('');
